59fe7e1c15c6bffe125c188d7e676303cf81887d
Brent Hsieh <horus882@gmail.com>
2020-08-25 13:01:07 +0800

    新增 meta 圖, 調整 menu 下方留白

--- include/head.php
+++ include/head.php
@@ -12,8 +12,12 @@
 <meta name="viewport" content="width=480, viewport-fit=cover">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+<link rel="icon" sizes="192x192" href="/images/common/webicon.png">
+<link rel="apple-touch-icon-precomposed" href="/images/common/webicon.png">
+<link rel="shortcut icon" type="image/x-icon" href="/images/common/favicon.ico">
+
 <meta name="description" content="KIRIN會員至「全家便利商店」購買冰結旅途調酒，RIMOWA旅行箱與LINE POINTS等你帶回家！">
-<meta property="og:image" content="">
+<meta property="og:image" content="https://www.kirin.com.tw/hyoketsu/event/tabisuru2020/meta.jpg">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="630">
 <meta property="og:description" content="KIRIN會員至「全家便利商店」購買冰結旅途調酒，RIMOWA旅行箱與LINE POINTS等你帶回家！">
